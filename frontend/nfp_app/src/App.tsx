4806bce14cfe5727e8391f93303d2edc4e8bf66b
qiy-chen <92070091+qiy-chen@users.noreply.github.com>
2025-01-28 20:46:06 -0500

    Fixed BrowserRouting

--- frontend/nfp_app/src/App.tsx
+++ frontend/nfp_app/src/App.tsx
@@ -9,21 +9,16 @@ import Categories from "./pages/Categories";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Routes>
-          {/* Wrap routes with the Layout */}
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="reports" element={<ReportsPage />} />
-            <Route path="categories" element={<Categories />} />
-            <Route path="*" element={<div>Page not found</div>} />
-          </Route>
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <Routes>
+      {/* Wrap routes with the Layout */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="reports" element={<ReportsPage />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="*" element={<div>Page not found</div>} />
+      </Route>
+    </Routes>
   );
 }
 
